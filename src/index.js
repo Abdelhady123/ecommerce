d33b61b5ad8d2be4d84c780e768d65a827861d64
abdalhady almajaly <mjalybdalhady@gmail.com>
2025-02-11 17:56:40 +0300

    solve some proplem done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './sass/style.scss';
 //اضهار العنوان المنبثق عند التمرير على عربة الشراء
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
- 
 
 // message on click add-to-cart-btn
 document.querySelectorAll('.add-to-cart-btn').forEach(item =>{
@@ -17,7 +16,6 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item =>{
 //product.html
 //radio button 
 document.querySelectorAll('.size-option input[type="radio"]').forEach(item =>{
-
     item.addEventListener('change',() =>{
         document.querySelectorAll('.size-option').forEach(i => {
             i.classList.remove('active')
@@ -26,7 +24,6 @@ document.querySelectorAll('.size-option input[type="radio"]').forEach(item =>{
     })
 })
 document.querySelectorAll('.color-option input[type="radio"]').forEach(item =>{
-
     item.addEventListener('change',() =>{
         document.querySelectorAll('.color-option').forEach(i => {
             i.classList.remove('active')
